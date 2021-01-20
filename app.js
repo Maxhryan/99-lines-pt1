eb29defda1afd00f5574851903ccedeb8c87aa62
Max <maxhryan@outlook.com>
2021-01-20 14:00:18 -0600

    updated finished project

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 let friends = ["john", "jane", "mike", "max", "dave"];
 
 for( let f = 0; f < friends.length; f++){
+    console.log(friends[f])
     for (let i = 99; i > 0; i--) {
-    if(i > 1){
+        if(i === 2){
+            console.log(`${i} lines of code in the file, ${i} lines of code; ${friends[f]} strikes one out, clears it all out, ${i-1} line of code in the file`)
+        }else if(i > 1){
         console.log(`${i} lines of code in the file, ${i} lines of code; ${friends[f]} strikes one out, clears it all out, ${i-1} lines of code in the file`)
     }else{
         console.log(`${i} line of code in the file, ${i} line of code; ${friends[f]} strikes it out, clears it all out ${i-1} lines of code in the file`)
     }
 }
 }
-
