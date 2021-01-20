417c1e525ed9960ef2371d7ff56113752dd43b78
Max <maxhryan@outlook.com>
2021-01-19 20:45:02 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let friends = ["john","jane","mike","max","dave"]
+
+for(let i = 99; i > 0; i--){
+    if(i > 1){
+        console.log(`${i} lines of code in the file ${i} lines of code you strikes one out clears it all out ${i-1} lines of code in the file`);
+    }else{
+        console.log(`${i} line of code in the file ${i} line of code you strikes one out clears it all out ${i-1} lines of code in the file`)
+    }
+}
+
